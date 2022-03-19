937e629b2ab0f04c5b20df357bc09cd83bbd99b1
AFIQ AIMAN <afiqaiman091@gmail.com>
2022-03-19 11:49:34 +0800

    insert data Api only

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|max:191',
+            'description' => 'required|max:191',
+            'price' => 'required|max:191',
+            'qty' => 'required|max:191',
+
+        ]);
+        
+        $product = new Product;
+        $product->name = $request->name;
+        $product->description = $request->description;
+        $product->price = $request->price;
+        $product->qty = $request->qty;
+        $product->save();
+        return response()->json(['message' => 'Product Added Successfully'],200);
+    }
+}
