9c09c56ab728b9d43c4a116610139ed6180c1a2b
AFIQ AIMAN <afiqaiman091@gmail.com>
2022-03-27 23:56:02 +0800

    SANCTUM

--- routes/api.php
+++ routes/api.php
@@ -16,11 +16,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:sanctum'])->group(function(){
+    
+    Route::resource('posts',PostController::class);
 });
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 Route::get('products', [ProductController::class, 'index']);
 Route::post('products/add', [ProductController::class, 'store']);
-
-Route::resource('posts',PostController::class);
